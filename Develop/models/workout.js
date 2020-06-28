c3dbe1cc221d1e784d4a0f1fc3e02bfecd4ecaaa
Steven Acevedo <astevenaa@gmail.com>
2020-06-28 15:23:21 -0700

    workout model

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -3,7 +3,33 @@ const { Schema, model } = require("mongoose");
 const workoutSchema = new Schema({
   excercises: [
     {
-      type: {},
+      type: {
+        type: String,
+        required: "enter type of workout",
+      },
+      name: {
+        type: String,
+        required: "enter specific workout name",
+      },
+      duration: {
+        type: Number,
+        required: "enter how many minutes long is the workout",
+      },
+      weight: {
+        type: Number,
+        required: "enter the added weight used",
+      },
+      reps: {
+        type: Number,
+        required: "enter the number of reps performed",
+      },
+      sets: {
+        type: Number,
+        required: "enter number of sets completed",
+      },
     },
   ],
 });
+const Workout = model("Workout", workoutSchema);
+
+module.exports = Workout;
